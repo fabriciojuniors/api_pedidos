e1076d0e305fbf29a47bfa6dfe7ac8def89449bc
Junior Souza <fabriciojuniorsc@gmail.com>
2020-10-10 19:06:40 -0300

    Criando classe categoria e adicionando banco H2 para teste

---
+++ src/main/java/com/api_pedidos/resources/CategoriaResource.java
@@ -0,0 +1,26 @@
+package com.api_pedidos.resources;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.api_pedidos.domain.Categoria;
+
+@RestController
+@RequestMapping(value="/categorias")
+public class CategoriaResource {
+
+	@RequestMapping(method = RequestMethod.GET)
+	public List<Categoria> listar() {
+		Categoria c1 = new Categoria(1, "Informática");
+		Categoria c2 = new Categoria(2, "Escritório");
+		
+		List<Categoria> lista = new ArrayList<>();
+		lista.add(c1);
+		lista.add(c2);
+		return lista;
+	}
+}
