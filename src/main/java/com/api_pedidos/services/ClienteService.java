32ffca8a72491921a12a74d73d74a20cb6494873
Junior Souza <fabriciojuniorsc@gmail.com>
2020-10-11 15:07:01 -0300

    Endpoint cliente

---
+++ src/main/java/com/api_pedidos/services/ClienteService.java
@@ -0,0 +1,23 @@
+package com.api_pedidos.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.api_pedidos.domain.Cliente;
+import com.api_pedidos.repositories.ClienteRepository;
+import com.api_pedidos.services.exceptions.ObjectNotFoundException;
+
+
+@Service
+public class ClienteService {
+	
+	@Autowired
+	private ClienteRepository repo;
+	
+	public Cliente buscar(Integer id) throws ObjectNotFoundException {
+		Optional<Cliente> obj = repo.findById(id);
+		return obj.orElseThrow(()-> new ObjectNotFoundException("Objeto não encontrado! ID: " + id + " Tipo: " + Cliente.class.getName()));
+	}
+}
