5a5de5248c6e66c881bb99cfce7e5c6bec6cc08c
Junior Souza <fabriciojuniorsc@gmail.com>
2020-10-10 21:09:15 -0300

    Adicionando Estado e Cidade

--- src/main/java/com/api_pedidos/ApiPedidosApplication.java
+++ src/main/java/com/api_pedidos/ApiPedidosApplication.java
@@ -9,8 +9,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.api_pedidos.domain.Categoria;
+import com.api_pedidos.domain.Cidade;
+import com.api_pedidos.domain.Estado;
 import com.api_pedidos.domain.Produto;
 import com.api_pedidos.repositories.CategoriaRepository;
+import com.api_pedidos.repositories.CidadeRepository;
+import com.api_pedidos.repositories.EstadoRepository;
 import com.api_pedidos.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -21,6 +25,12 @@ public class ApiPedidosApplication implements CommandLineRunner {
 	
 	@Autowired
 	private ProdutoRepository produtoRepository;
+	
+	@Autowired
+	private CidadeRepository cidadeRepository;
+	
+	@Autowired
+	private EstadoRepository estadoRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(ApiPedidosApplication.class, args);
@@ -42,8 +52,20 @@ public class ApiPedidosApplication implements CommandLineRunner {
 		p2.getCategorias().addAll(Arrays.asList(c1, c2));
 		p3.getCategorias().addAll(Arrays.asList(c1));
 		
+		Estado e1 = new Estado(null, "Minas Gerais");
+		Estado e2 = new Estado(null, "São Paulo");
+		
+		Cidade cit1 = new Cidade(null, "Uberlandia", e1);
+		Cidade cit2 = new Cidade(null, "São Paulo", e2);
+		Cidade cit3 = new Cidade(null, "Campinas", e2);
+		
+		e1.getCidades().addAll(Arrays.asList(cit1));
+		e2.getCidades().addAll(Arrays.asList(cit2, cit3));
+		
 		categoriaRepository.saveAll(Arrays.asList(c1, c2));
 		produtoRepository.saveAll(Arrays.asList(p1,p2,p3));
+		estadoRepository.saveAll(Arrays.asList(e1, e2));
+		cidadeRepository.saveAll(Arrays.asList(cit1,cit2,cit3));
 		
 	}
 
