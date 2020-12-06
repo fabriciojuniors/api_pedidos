f69d54947d4522c3b1c02c82ce8d6426c353fc44
Junior Souza <fabriciojuniorsc@gmail.com>
2020-12-06 18:33:20 -0300

    Email HTML

--- src/main/java/com/api_pedidos/services/EmailService.java
+++ src/main/java/com/api_pedidos/services/EmailService.java
@@ -1,13 +1,19 @@
 package com.api_pedidos.services;
 
+import javax.mail.internet.MimeMessage;
+
 import org.springframework.mail.SimpleMailMessage;
 
 import com.api_pedidos.domain.Pedido;
 
 public interface EmailService {
-	
+
 	void sendOrderConfirmationEmail(Pedido pedido);
-	
+
 	void sendEmail(SimpleMailMessage msg);
+
+	void sendOrderConfirmationHtmlEmail(Pedido pedido);
+
+	void sendHtmlEmail(MimeMessage msg);
 
 }
